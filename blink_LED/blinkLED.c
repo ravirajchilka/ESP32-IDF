abcadb9ef7cf0c9f18277a4ff52af89d9236c81c
ravirajchilka <ravirajchilka10@gmail.com>
2023-06-19 23:25:02 -0400

    Blink LED

---
+++ blink_LED/blinkLED.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define PIN 2
+
+void app_main(void) {
+    gpio_set_direction(PIN, GPIO_MODE_OUTPUT);
+    uint32_t isOn = 0;
+
+    while(1) {
+        isOn = !isOn;
+        gpio_set_level(PIN,isOn);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+
+}
