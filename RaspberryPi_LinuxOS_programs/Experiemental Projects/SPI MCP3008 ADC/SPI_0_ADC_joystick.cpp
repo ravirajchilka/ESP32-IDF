198e8c1201ddf081aae70dfce1112a032181d342
Raviraj <ravirajchilka10@gmail.com>
2024-07-22 23:46:39 -0400

    Joystick controlling LEDs run direction

--- RaspberryPi_LinuxOS_programs/Experiemental Projects/SPI MCP3008 ADC/SPI_0_ADC_joystick.cpp
+++ RaspberryPi_LinuxOS_programs/Experiemental Projects/SPI MCP3008 ADC/SPI_0_ADC_joystick.cpp
@@ -6,11 +6,27 @@
 #include <cstring>
 #include <chrono>
 #include <thread>
+#include <gpiod.hpp>
 
+// spi setup
 const char* spiDevice = "/dev/spidev0.0";
 uint8_t mode = SPI_MODE_0;
 uint8_t bits = 8;  // Changed to 8 bits
 uint32_t speed = 1000000;  // 1 MHz, adjust as needed
+
+// gpio pins setup
+const char* gpiopathname = "/dev/gpiochip0";
+const uint8_t offset_yellow = 17;
+const uint8_t offset_green = 27;
+const uint8_t offset_red = 22;
+const auto consumer = std::string("led run");
+
+gpiod::chip chip(gpiopathname);
+
+gpiod::line line_yellow = chip.get_line(offset_yellow);
+gpiod::line line_green = chip.get_line(offset_green);
+gpiod::line line_red = chip.get_line(offset_red);
+
 
 int spi_fd;
 
@@ -67,16 +83,64 @@ uint16_t readADC() {
     return ((rx[1] & 0x03) << 8) | rx[2];
 }
 
+// gpio pin reset for led pins
+void setLEDsToZero() {
+    line_yellow.set_value(0);
+    line_green.set_value(0);
+    line_red.set_value(0);
+}
+
 int main() {
     if (!spi_init()) {
         return -1;
     }
+
+    // gpio pins direction setup
+    line_yellow.request({consumer,gpiod::line_request::DIRECTION_OUTPUT},0);
+    line_green.request({consumer,gpiod::line_request::DIRECTION_OUTPUT},0);
+    line_red.request({consumer,gpiod::line_request::DIRECTION_OUTPUT},0);
 
     while(1) {
         uint16_t adcValue = readADC();
         float voltage = (adcValue / 1023.0) * 3.3;  // Assuming 3.3V reference
         std::cout << "ADC Value: " << adcValue << ", Voltage: " << voltage << "V" << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Read every second
+	    
+	try {
+	    if(voltage > 2) {
+            line_yellow.set_value(1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            setLEDsToZero();
+
+            line_green.set_value(1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            setLEDsToZero();
+            
+            line_red.set_value(1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            setLEDsToZero();
+
+	    } else if(voltage < 1) {
+            line_red.set_value(1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            setLEDsToZero();
+
+            line_green.set_value(1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            setLEDsToZero();
+            
+            line_yellow.set_value(1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            setLEDsToZero();
+
+	    } else {
+		    setLEDsToZero();
+	    }   
+	}
+	catch(const std::exception& e) {
+	    std::cerr << e.what();
+	}	
+
     }
 
     spi_close();
